43cb1370b378220abf8d879325e2f49c53b91884
iaia <iaia72160@gmail.com>
2019-06-25 16:20:07 +0900

    First commit

---
+++ src/Application.kt
@@ -0,0 +1,21 @@
+package com.example
+
+import io.ktor.application.*
+import io.ktor.features.ContentNegotiation
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.get
+import io.ktor.routing.routing
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/HelloWorld") {
+            call.respondText("Hello")
+        }
+    }
+}
+
